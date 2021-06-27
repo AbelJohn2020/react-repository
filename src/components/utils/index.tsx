851c7d299774335cb12d760060bca663066a6a0f
Abel AR <abel_john_14@hotmail.com>
2021-06-26 20:37:19 -0500

    Request with fetch plus then and catch

--- src/components/utils/index.tsx
+++ src/components/utils/index.tsx
@@ -1,12 +1,23 @@
 import { base_uri } from "../../app/config"
 
 export const getCharacter = async () => {
-    try {
-        const url = await fetch(base_uri);
-        const data = await url.json();
-        const results = data.results;
-        console.log(results)
-    } catch(e) {
-        console.log(e)
-    }
+    // try {
+    //     const url = await fetch(base_uri);
+    //     const data = await url.json();
+    //     const results = data.results;
+    //     console.log(results)
+    // } catch(e) {
+    //     console.log(e)
+    // }
+
+    fetch(base_uri)
+        .then( res => {
+            console.log(res);
+            return res.json();
+        })
+        .then( res => {
+            const data = res.results
+            console.log(data)
+        })
+        .catch( e => console.log(e))
 }
