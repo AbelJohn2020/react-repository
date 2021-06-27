c9ad6ba87c04eedd7696b802a8bfde390e33be74
Abel AR <abel_john_14@hotmail.com>
2021-06-26 20:57:51 -0500

    Request with axios plus then and catch

--- src/components/utils/index.tsx
+++ src/components/utils/index.tsx
@@ -1,3 +1,4 @@
+import axios from "axios"
 import { base_uri } from "../../app/config"
 
 export const getCharacter = async () => {
@@ -10,14 +11,29 @@ export const getCharacter = async () => {
     //     console.log(e)
     // }
 
-    fetch(base_uri)
-        .then( res => {
-            console.log(res);
-            return res.json();
-        })
-        .then( res => {
-            const data = res.results
-            console.log(data)
-        })
-        .catch( e => console.log(e))
+    // fetch(base_uri)
+    //     .then( res => {
+    //         console.log(res);
+    //         return res.json();
+    //     })
+    //     .then( res => {
+    //         const data = res.results
+    //         console.log(data)
+    //     })
+    //     .catch( e => console.log(e))
+
+
+    // WITH AXIOS:
+
+    // axios.get(base_uri)
+    //     .then((res) => {
+    //         const data = res.data;
+    //         const results = data.results
+    //         console.log(results);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err)
+    //     });
+
+    // with try and catch
 }
